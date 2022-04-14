c07ae966d76e6e823b236e3b719fbc0ffa612790
Dilgam Israfilov <dilqemisrafil@gmail.com>
2022-04-14 22:52:24 +0400

    finding odd number

---
+++ odd.js
@@ -0,0 +1,11 @@
+const isOdd  = function (num) {
+  return num % 2 === 1;
+}
+
+//const tenIsEven = isEven(10);
+//const elevenIsEven = isEven(11);
+
+console.log("3 is odd:" + isOdd(3));
+console.log("8 is odd:" + isOdd(8));
+
+
